a980002f0d495cc8fb91af0be04f610d0424541d
Luiz Augusto <luiz.zillmann@engenharia.ufjf.br>
2017-11-17 10:22:45 -0200

    Batch Norm

--- codigo teste/tools/conv/shallownet.py
+++ codigo teste/tools/conv/shallownet.py
@@ -5,35 +5,41 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras import backend as K
+from keras.layers.normalization import BatchNormalization
+
 
 class ShallowNet:
-	@staticmethod
-	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last"
-		model = Sequential()
-		inputShape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last"
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
 
-		# define the first (and only) CONV => RELU layer
-		model.add(Conv2D(32, (2, 2), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
+        # define the first (and only) CONV => RELU layer
+        model.add(Conv2D(32, (2, 2), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
 
-		# define the second CONV => RELU layer VINICIUS
-		model.add(Conv2D(64, (2, 2), padding="same",
-						 input_shape=inputShape))
-		model.add(Activation("relu"))
+        # define the second CONV => RELU layer VINICIUS
+        model.add(Conv2D(64, (2, 2), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
 
-		# Camada so para tentar regular e softmax classifier
-		model.add(Flatten())
-		# model.add(Dense(10))
-		# model.add(Activation("relu"))
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # Camada so para tentar regular e softmax classifier
+        model.add(Flatten())
+        # model.add(Dense(10))
+        # model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
