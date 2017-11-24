25235e0cb732256833ad55cb7cc89e21144b5b11
viniciusvidal2 <vinicius.vidal@engenharia.ufjf.br>
2017-11-24 09:40:32 -0200

    Ajuste da janela e nota de necessidade de mais negativos

--- codigo/reconhece_poste.py
+++ codigo/reconhece_poste.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import load_model
 from cv2 import *
+import time
 # Ajustar o tamanho da imagem ao tamanho da entrada da rede
 from keras.preprocessing.image import img_to_array
 
@@ -11,10 +12,10 @@ def scales(windowSize_base):
 	for scale in range(1, 3, 1):
 		yield windowSize_base*scale # precisa ser array
 
-def sliding_window(image, stepSize, windowSize):
+def sliding_window(image, stepSizeW, stepSizeH, windowSize):
 	# slide a window across the image
-	for y in range(0, image.shape[0], stepSize):
-		for x in range(0, image.shape[1], stepSize):
+	for y in range(0, image.shape[0], stepSizeH):
+		for x in range(0, image.shape[1], stepSizeW):
 			# yield the current window
 			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
@@ -29,23 +30,29 @@ def show_posts(foto, xp, yp, wsp):
 model = load_model("Melhores_redes/melhor_atual.hdf5") # Cuidado com a manipulacao do arquivo
 
 # Carregando imagem de teste
-# foto = imread("/home/vinicius/Desktop/Deep_Learning/datasets/Testes/frame0003.jpg")
-foto = imread("/home/vinicius/Desktop/Deep_learning/datasets/Testes/frame0003.jpg")
+foto = imread("/home/vinicius/Desktop/Deep_Learning/datasets/Testes/frame0351.jpg")
+# foto = imread("/home/vinicius/Desktop/Deep_learning/datasets/Testes/frame0003.jpg")
 # imshow("teste ok", foto)
 # waitKey(0)
 # destroyAllWindows()
 
 # Definindo janela inicial
-window_size = np.array((60, 190))
+window_size = np.array((65, 190))
 
 # Guardando locais onde se encontra o poste
-x_p = []; y_p = []; window_size_p = [];
+x_p = []; y_p = []; window_size_p = []
 
 print("[INFO] Comecando a varrer a foto...")
 # Varrendo a foto escolhida
 for sc in scales(window_size):
-	step_size = foto.shape[0]//window_size[0]
-	for (x, y, window) in sliding_window(foto, step_size, window_size):
+	# Calculando passo sobre a foto
+	many_steps_w = 2*foto.shape[0]//sc[0]
+	step_size_w  = foto.shape[0]//many_steps_w
+
+	many_steps_h = 2*foto.shape[1]//sc[1]
+	step_size_h  = foto.shape[1]//many_steps_h
+
+	for (x, y, window) in sliding_window(foto, step_size_w, step_size_h, window_size):
 		# AJUSTAR A IMAGEM DE ENTRADA
 		window = resize(window, (65, 190))
 		window = window.astype("float")/255.0
@@ -59,13 +66,14 @@ for sc in scales(window_size):
 			window_size_p.append(window_size)
 
 		clone = foto.copy()
-		# rectangle(clone, (x,y), (x + window_size[0], y + window_size[1]), (0,255,0),2)
-		# imshow("Slide", clone)
-		# waitKey(1)
-		# time.sleep(0.025)
+		rectangle(clone, (x,y), (x + window_size[0], y + window_size[1]), (0,255,0),2)
+		imshow("Slide", clone)
+		waitKey(1)
+		time.sleep(0.025)
 
+destroyWindow("Slide")
 # Onde estao os postes Luiz?
 print("[INFO] Foto varrida, esses sao os postes...")
-show_posts(foto=clone, xp=x_p, yp=y_p, wsp=window_size_p)
+show_posts(foto=foto, xp=x_p, yp=y_p, wsp=window_size_p)
 
 espresso = 1
